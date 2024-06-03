01fe2e16f35e5b1fb2ce7c8451767bd09d9a6051
Milton Palmeira <lpsantana_@hotmail.com>
2024-06-03 08:02:38 -0300

    users crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,11 @@
-import { createUser } from "../models/User.js";
+import {
+  createUser,
+  createUserWithPost,
+  deleteUser,
+  findUserByEmail,
+  findUsers,
+  updateUser,
+} from "../models/User.js";
 
 export const addUser = async (req, res) => {
   try {
@@ -9,5 +16,65 @@ export const addUser = async (req, res) => {
     res
       .status(500)
       .json({ error: "Failed to add users", message: error.message });
+  }
+};
+
+export const findAllUsers = async (req, res) => {
+  try {
+    const users = await findUsers();
+    res.status(200).json({ users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to find users", message: error.message });
+  }
+};
+
+export const getUserByEmail = async (req, res) => {
+  try {
+    const user = await findUserByEmail(req.params.email);
+    res.status(200).json({ user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to find user by email", message: error.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { email } = req.params.email;
+    const userData = req.body;
+    const user = await updateUser(email, userData);
+    res.status(200).json({ user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update user", message: error.message });
+  }
+};
+
+export const delUser = async (req, res) => {
+  try {
+    const { email } = req.params.email;
+    const user = await deleteUser(email);
+    res.status(200).json({ user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to delete user", message: error.message });
+  }
+};
+
+export const createUserPost = async (req, res) => {
+  try {
+    const { userData, postData } = req.body;
+    const user = await createUserWithPost(userData, postData);
+    res.status(201).json({ user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to add users with post", 
+        message: error.message });
   }
 };
--- src/models/User.js
+++ src/models/User.js
@@ -10,8 +10,59 @@ export const createUser = async (name, email) => {
     });
   } catch (error) {
     if (error.code == "P2002") {
-        throw new Error("Email address already exists.");
+      throw new Error("Email address already exists.");
     }
     throw new Error(`Failed to create user: ${error.message}`);
   }
+};
+
+export const findUsers = async () => {
+  try {
+    return await prisma.user.findMany();
+  } catch (error) {
+    throw new Error(`Failed to get all users: ${error.message}`);
+  }
+};
+
+export const findUserByEmail = async (email) => {
+  return await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+};
+
+export const updateUser = async (email, userData) => {
+  return await prisma.user.update({
+    where: {
+      email,
+    },
+    data: {
+      name: userData.name,
+      status: userData.status,
+    },
+  });
+};
+
+export const deleteUser = async (email) => {
+  return await prisma.user.delete({
+    where: {
+      email,
+    },
+  });
+};
+
+export const createUserWithPost = async (dataUser, dataPost) => {
+  return await prisma.user.create({
+    data: {
+      name: dataUser.name,
+      email: dataUser.email,
+      Post: {
+        create: {
+          title: dataPost.title,
+          body: dataPost.body,
+        },
+      },
+    },
+  });
 };
